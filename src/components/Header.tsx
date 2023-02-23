1098ad4abb12544d7d5392a3559bbb69d1de6ce3
reid501 <alexatrs@hotmail.co.uk>
2023-02-23 18:17:44 +0000

    styles added to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,16 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
+    <header className="bg-slate-500 flex justify-center py-5">
+      <nav className="w-4/5 justify-between flex ">
+        <div>
+          <h1 className="text-slate-200">MTB</h1>
+        </div>
+        <ul className="flex">
+          <li className="text-slate-200">
             <Link href="/">Home</Link>
+          </li>
+          <li className="text-slate-200 ml-10">
             <Link href="/news">News</Link>
           </li>
         </ul>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="bg-red-500">
-      <h1>MTB</h1>
-    </div>
-  );
+  return <></>;
 }
